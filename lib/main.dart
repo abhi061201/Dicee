6b6d8eb305611ad90114ccbdd4638950bb81d73f
abhi061201 <72760503+abhi061201@users.noreply.github.com>
2021-09-05 10:12:13 +0530

    enter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -8,6 +9,7 @@ void main() {
         appBar: AppBar(
           title: Text('Dicee'),
           backgroundColor: Colors.red,
+          centerTitle: true,
         ),
         body: DicePage(),
       ),
@@ -15,9 +17,73 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  // var leftDiceButton=1;
+  int randomise(){
+  int l=Random().nextInt(6)+1;
+  return l;
+}
+  int leftDiceButton = 1;
+  int rightDiceButton = 1;
   Widget build(BuildContext context) {
-    return Container();
+    // Variables should be define in Build method in order to use hot reload
+    // leftDiceButton = 2;
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    // if the random no. is not present in dice no. than dice will disapeer
+                    // leftDiceButton = Random().nextInt(6) + 1;
+                    //  // if both want to update
+                    // rightDiceButton= Random().nextInt(6)+1;
+                    leftDiceButton=randomise();
+                    rightDiceButton=randomise();
+                  });
+
+                  // print('Left Button is pressed');
+                  print(leftDiceButton);
+                },
+                child: Image.asset('images/dice$leftDiceButton.png'),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    //......
+                    // rightDiceButton = Random().nextInt(6) + 1;
+                    // // if both want to update
+                    // leftDiceButton= Random().nextInt(6)+1;
+                    leftDiceButton=randomise();
+                    rightDiceButton=randomise();
+                  });
+
+                  print('Right Button is Pressed');
+                },
+                child: Image(
+                  image: AssetImage(
+                    'images/dice$rightDiceButton.png',
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
+
